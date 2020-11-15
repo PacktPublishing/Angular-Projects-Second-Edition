0687d291c4cfa444711091d9408707f5ab127c55
Aristeidis Bampakos <bampakoa@gmail.com>
2020-11-15 13:27:43 +0200

    Add articles feature

--- CH2/src/app/app-routing.module.ts
+++ CH2/src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
+  { path: '', pathMatch: 'full', redirectTo: 'articles' },
+  { path: '**', redirectTo: 'articles' }
 ];
 
 @NgModule({
---
+++ CH2/src/app/articles/articles.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ArticlesRoutingModule } from './articles-routing.module';
+import { ArticlesComponent } from './articles.component';
+
+
+@NgModule({
+  declarations: [ArticlesComponent],
+  imports: [
+    CommonModule,
+    ArticlesRoutingModule
+  ]
+})
+export class ArticlesModule { }
