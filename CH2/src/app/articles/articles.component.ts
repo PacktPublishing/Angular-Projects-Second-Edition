faf2676476d0a53e0e66446985c4de902f0b1dd0
Aristeidis Bampakos <bampakoa@gmail.com>
2020-12-07 00:27:00 +0200

    Unsubscribe from available$ observable

--- CH2/src/app/articles/articles.component.ts
+++ CH2/src/app/articles/articles.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent implements OnInit {
+export class ArticlesComponent implements OnInit, OnDestroy {
 
   posts: ScullyRoute[];
+  private routeSub: Subscription;
 
   constructor(private scullyService: ScullyRoutesService) { }
 
   ngOnInit(): void {
-    this.scullyService.available$.subscribe(posts => {
+    this.routeSub = this.scullyService.available$.subscribe(posts => {
       this.posts = posts.filter(post => post.title);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
   }
 
 }
