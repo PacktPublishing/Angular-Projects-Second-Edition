dfc700b852f52fbc65e074c8fb3ccff222a4e7f6
Aristeidis Bampakos <bampakoa@gmail.com>
2021-03-04 01:57:24 +0200

    Add SEO services

--- CH7/src/app/app.component.ts
+++ CH7/src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from '../environments/environment';
+import { Title } from '@angular/platform-browser';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   username = environment.username;
+
+  constructor(private title: Title, private meta: Meta) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('GitHub portfolio app');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: `${this.username}'s GitHub portfolio`
+      },
+      {
+        name: 'author',
+        content: this.username
+      }
+    ]);
+  }
 }
