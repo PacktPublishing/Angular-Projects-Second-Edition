ed4b699159296460005a98e3dd38b45cf3a65f6b
Aristeidis Bampakos <bampakoa@gmail.com>
2021-03-24 21:23:22 +0200

    Add the visitor component

--- CH8/apps/tour/src/app/app.module.ts
+++ CH8/apps/tour/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: '', pathMatch: 'full', redirectTo: 'tour' },
+      { path: 'tour', loadChildren: () => import('@packt/visitor').then(m => m.VisitorModule) }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- CH8/libs/visitor/src/lib/visitor.module.ts
+++ CH8/libs/visitor/src/lib/visitor.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VisitorComponent } from './visitor.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,
+    RouterModule.forChild([
+      { path: '', component: VisitorComponent }
+    ])
+  ],
+  declarations: [VisitorComponent],
 })
 export class VisitorModule {}
