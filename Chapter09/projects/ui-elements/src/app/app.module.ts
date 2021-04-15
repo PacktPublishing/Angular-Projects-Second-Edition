3c2da61262c07a1f434d610ee00a5cb9f2491ad0
Aristeidis Bampakos <bampakoa@gmail.com>
2021-04-15 23:12:40 +0300

    Create Angular elements

---
+++ Chapter09/projects/ui-elements/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+import { UiControlsModule, CopyButtonComponent } from 'ui-controls';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    UiControlsModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule {
+
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap(): void {
+    const el = createCustomElement(CopyButtonComponent, { injector: this.injector });
+    customElements.define('copy-button', el);
+  }
+}
